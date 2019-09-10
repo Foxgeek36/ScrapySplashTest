1479e26228c19aed71706c10217859f95a32ea40
Foxgeek36 <1002301246@qq.com>
2019-09-11 00:25:58 +0800

    third commit

--- scrapysplashtest/spiders/taobao.py
+++ scrapysplashtest/spiders/taobao.py
@@ -2,8 +2,10 @@
 from scrapy import Spider, Request
 from urllib.parse import quote
 from scrapysplashtest.items import ProductItem
-from scrapy_splash import SplashRequest
+from scrapy_splash import SplashRequest  # attention +--
 
+# Lua脚本/ 实现页面加载&模拟点击翻页的操作 +--
+# -注重实现及写法操作/ 最后返回的是页面源码 splash:html()
 script = """
 function main(splash, args)
   splash.images_enabled = false
@@ -18,6 +20,9 @@ end
 
 
 class TaobaoSpider(Spider):
+    '''
+    splash为异步执行,比selenium更高效更适合多机部署
+    '''
     name = 'taobao'
     allowed_domains = ['www.taobao.com']
     base_url = 'https://s.taobao.com/search?q='
@@ -26,19 +31,22 @@ class TaobaoSpider(Spider):
         for keyword in self.settings.get('KEYWORDS'):
             for page in range(1, self.settings.get('MAX_PAGE') + 1):
                 url = self.base_url + quote(keyword)
-                yield SplashRequest(url, callback=self.parse, endpoint='execute',
-                                    args={'lua_source': script, 'page': page, 'wait': 7})
+                # attention: endpoint接口为'execute'
+                # -Splash综合所有参数对页面做渲染,返回的response为渲染之后的结果,最后交给Spider(parse)做解析 +--
+                yield SplashRequest(url,
+                                    args={'lua_source': script, 'page': page, 'wait': 7},  # attention +--
+                                    endpoint='execute',
+                                    callback=self.parse)
     
     def parse(self, response):
-        products = response.xpath(
-            '//div[@id="mainsrp-itemlist"]//div[@class="items"][1]//div[contains(@class, "item")]')
+        products = response.xpath('//div[@id="mainsrp-itemlist"]//div[@class="items"][1]//div[contains(@class, "item")]')
         for product in products:
             item = ProductItem()
             item['price'] = ''.join(product.xpath('.//div[contains(@class, "price")]//text()').extract()).strip()
             item['title'] = ''.join(product.xpath('.//div[contains(@class, "title")]//text()').extract()).strip()
             item['shop'] = ''.join(product.xpath('.//div[contains(@class, "shop")]//text()').extract()).strip()
-            item['image'] = ''.join(
-                product.xpath('.//div[@class="pic"]//img[contains(@class, "img")]/@data-src').extract()).strip()
+            item['image'] = ''.join(product.xpath('.//div[@class="pic"]//img[contains(@class, "img")]/@data-src').extract()).strip()
             item['deal'] = product.xpath('.//div[contains(@class, "deal-cnt")]//text()').extract_first()
             item['location'] = product.xpath('.//div[contains(@class, "location")]//text()').extract_first()
-            yield item
+
+            yield item  # 直接传递至pipelines中做处理
